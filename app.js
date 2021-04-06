93cd90d796f9d146ff62940748534a3208bf084b
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2021-04-06 14:37:38 +0200

    add github

---
+++ app.js
@@ -0,0 +1,148 @@
+// Modules
+let express   = require('express');
+let http      = require('http');
+let socketIo  = require('socket.io');
+let morgan    = require('morgan');
+let striptags = require('striptags');
+let config    = require('./config.json');
+const cors = require('cors');
+
+// Constantes
+const ex     = express();
+const app  = http.createServer(ex);
+const io      = socketIo(app);
+const port    = config.express.port;
+const options = {
+    root: __dirname+'/views'
+}
+
+// Variables globales
+let usernames = []
+
+// Middlewares
+ex.use(express.static(options.root))
+ex.use(morgan('dev'))
+ex.use(cors());
+
+ex.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Routes
+
+ex.get('/', (req, res) => {
+    res.redirect('/home')
+})
+
+ex.get('/home', (req, res) => {
+    res.sendFile('index.html', options)
+})
+
+ex.get('/params/:name', (req, res) => {
+    res.send(req.params.name)
+})
+
+// IO
+
+io.on('connection', socket => {
+
+    console.log('a user connected : ' + socket.id);
+
+    // Traitement pour l'assignation d'un username
+    socket.on('setUsername', (usernameWanted) => {
+
+        // Traitement de la chaine de caractères
+        usernameWanted = striptags(usernameWanted.trim());
+
+        // Vérification de l'unicité de l'username
+        let usernameTaken = false
+        for (let socketid in usernames) {
+            if (usernames[socketid] === usernameWanted)
+                usernameTaken = true
+        }
+
+        let timeFakeLoading = 0;
+       // setTimeout(() => {
+
+            // Traitement final
+            if (usernameTaken) {
+                socket.emit('rejectUsername', usernameWanted);
+            } else {
+                socket.join('users', () => {
+                    usernames[socket.id] = usernameWanted;
+                    let justUsernames = getUsernames();
+                    socket.emit('acceptUsername', usernameWanted, justUsernames, getSocketIDs());
+                    socket.to('users').emit('newUser', usernameWanted, socket.id, justUsernames);
+                })
+            }
+            
+        //});
+    });
+
+    //Reception d'un message
+
+    socket.on('sendMessage', (text, dataChat) => {
+        text = striptags(text.trim());
+        if(text !== ''){
+            let data = getVariablesDataChat(dataChat, socket.id);
+            socket.to(data.roomToSend).emit('newMessage', text, usernames[socket.id], data.chatToShow);
+            socket.emit('confirmMessage', text, data.dataChat);
+        }
+
+    })
+
+    //information ebn cour d'ceriture
+    socket.on('startWriting', (dataChat) => {
+        let data = getVariablesDataChat(dataChat, socket.id);
+        socket.to(data.roomToSend).emit('userStartWriting', usernames[socket.id], data.chatToShow);
+    })
+    socket.on('stopWriting', (dataChat) => {
+        let data = getVariablesDataChat(dataChat, socket.id);
+        socket.to(data.roomToSend).emit('userStopWriting', data.chatToShow)
+    })
+
+    // Déconnexion de l'utilisateur
+    socket.on('disconnect', () => {
+        if (usernames[socket.id]) {
+            delete usernames[socket.id]
+            socket.to('users').emit('leftUser', socket.id, getUsernames())
+        }
+    })
+
+});
+
+
+
+// Renvoie un array contenant uniquement les usernames sans index
+function getUsernames() {
+    let users = []
+    for (let u in usernames) {
+        users.push(usernames[u])
+    }
+    return users
+}
+
+// Renvoie un array contenant uniquement les socketId sans index
+function getSocketIDs() {
+    let socketIDs = []
+    for (let socketid in usernames) {
+        socketIDs.push(socketid)
+    }
+    return socketIDs;
+}
+
+function getVariablesDataChat(dataChat, socketID){
+    dataChat = dataChat == null ? 'person0' : dataChat;
+    return {
+        roomToSend: dataChat === 'person0' ? 'users' : dataChat,
+        chatToShow: dataChat === 'person0' ? dataChat : socketID,
+        dataChat: dataChat
+    }
+
+
+}
+// Lancement de l'application
+ex.listen(3300, () => console.log(`Server started on port : ${3300}`));
---
+++ views/assets/js/index.js
@@ -0,0 +1,171 @@
+document.querySelector('.chat[data-chat=person0]').classList.add('active-chat');
+document.querySelector('.person[data-chat=person0]').classList.add('active');
+
+var friends = {
+  list: document.querySelector('ul.people'),
+  all: document.querySelectorAll('.left .person'),
+  name: '' },
+
+chat = {
+  container: document.querySelector('.container .right'),
+  current: null,
+  person: null,
+  name: document.querySelector('.container .right .top .name') };
+
+
+updateFriends();
+
+checkLinkUsersModal()
+
+function setAciveChat(f) {
+  friends.list.querySelector('.active').classList.remove('active');
+  f.classList.add('active');
+  chat.current = chat.container.querySelector('.active-chat');
+  chat.person = f.getAttribute('data-chat');
+  chat.current.classList.remove('active-chat');
+  chat.container.querySelector('[data-chat="' + chat.person + '"]').classList.add('active-chat');
+  friends.name = f.querySelector('.name').innerText;
+  chat.name.innerHTML = friends.name;
+  checkLinkUsersModal()
+}
+
+// Vérification de l'usabilité du lien vers le modal users
+function checkLinkUsersModal() {
+  if (chat.person == 'person0' || chat.person == null) {
+    chat.name.addEventListener('click', openUsersModal, false)
+    chat.name.style.cursor = 'pointer'
+  } else {
+    chat.name.removeEventListener('click', openUsersModal, false)
+    chat.name.style.cursor = 'default'
+  }
+}
+
+/* Modal */
+
+var modalContainer = document.body.querySelector('#modal-container'),
+    listUsers = modalContainer.querySelector('#contentUsers ul')
+
+// Ouverture du modal sur la demande de l'username
+function openUsernameModal() {
+  modalContainer.querySelector('#contentUsername').classList.remove('none')
+  modalContainer.querySelector('#contentUsers').classList.add('none')
+  modalContainer.classList.remove('out')
+  document.body.classList.add('modal-active')
+}
+
+// Ouverture du modal sur la liste des utilisateurs connectés
+function openUsersModal() {
+  modalContainer.querySelector('#contentUsername').classList.add('none')
+  modalContainer.querySelector('#contentUsers').classList.remove('none')
+  modalContainer.classList.remove('out')
+  document.body.classList.add('modal-active')
+}
+
+// Fermeture du modal
+function closeModal() {
+  //modalContainer.classList.add('out')
+  modalContainer.style.display = 'none';
+  document.body.classList.remove('modal-active')
+}
+
+// Mise à jour des utilisateurs présents
+function updateUsers(users) {
+
+  // Mise à jour du modal listant les users
+  listUsers.innerHTML = ''
+  for (let i in users) {
+    listUsers.innerHTML += '<li>'+users[i]+'</li>'
+  }
+
+  // Mise à jour du texte à gauche
+  let text = 'Discussion générale (' + users.length + ')'
+  friends.all[0].querySelector('.name').innerHTML = text
+
+  // Mise à jour du texte en haut (si c'est le chat actif)
+  if (chat.person == 'person0' || chat.person == null)
+    document.body.querySelector('#infoPersonTop').innerHTML = text
+
+}
+
+//Message
+let globalChat = chat.container.querySelector('.chat[data-chat=person0]');
+
+// Message d'un nouvel utilisateur
+function messageNewUser(newUsername, dataChat) {
+  let message = '<div class="conversation-start"><span>'+newUsername+' est là !</span></div>';
+  globalChat.insertAdjacentHTML('beforeend', message);
+}
+
+//message de l'utilisateur courant
+function showMyMessage(text, dataChat){
+  let message = '<div class="bubble name me">'+text+'</div>';
+  chat.container.querySelector('.chat[data-chat="' + dataChat + '"]').insertAdjacentHTML('beforeend', message);
+}
+
+//message de l'utilisateur exterieur
+function showNewMessage(text, usernameSender){
+  let message = '<div class="bubble name you"><span class="username">'+usernameSender+'</span>'+text+'</div>';
+  chat.container.querySelector('.chat[data-chat="' + dataChat + '"]').insertAdjacentHTML('beforeend', message);
+}
+
+var someoneWriting = document.body.querySelector('.someoneWriting');
+function showSomeoneWriting(usernameWriting, dataChat){
+  if (chat.person === dataChat || dataChat === 'person0' && chat.person == null){
+    someoneWriting.innerHTML = usernameWriting + ' est entrain d\'écrire...';
+    someoneWriting.classList.remove('none');
+  }
+}
+
+function removeSomeoneWriting(){
+  if (chat.person === dataChat || dataChat === 'person0' && chat.person == null){
+    someoneWriting.classList.add('none');
+  }
+
+}
+
+function setFriends(users, socketIDs, personalUsername){
+  for(let i = 0; i < users.length; i++ ){
+    if(personalUsername !== users[i]){
+      addUserChat(users[i], socketIDs[i] );
+    }
+  }
+}
+
+//messageries
+function updateFriends(){
+  friends.all = document.querySelectorAll('.left .person');
+  friends.all.forEach(function (f) {
+    f.addEventListener('mousedown', function () {
+      f.classList.contains('active') || setAciveChat(f);
+    });
+  });
+}
+
+//message privé
+function addUserChat(newUsername, newSocketID){
+  let element = '<li class="person" data-chat="'+newSocketID+'">\n' +
+      '                    <span class="name">'+newUsername+'</span><br />\n' +
+      '                    <span class="preview">message privé !</span>\n' +
+      '                </li>';
+  friends.list.insertAdjacentHTML('beforeend', element);
+  element = '<div class="chat" data-chat="' + newSocketID + '"></div>';
+  let lastChat = chat.container.querySelectorAll('.chat');
+  lastChat = lastChat[lastChat.length-1];
+  lastChat.insertAdjacentHTML('afterend', element);
+  updateFriends();
+}
+
+function removeUserChat(oldSocketID){
+
+  if(chat.person == oldSocketID){
+    setAciveChat(friends.all[0]);
+  }
+
+  let element = friends.list.querySelector('.person[data-chat="'+oldSocketID+'"]');
+  element.parentNode.removeChild(element);
+
+  element = chat.container.querySelector('.chat[data-chat="'+oldSocketID+'"]');
+  element.parentNode.removeChild(element);
+
+  updateFriends();
+}
